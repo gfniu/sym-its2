2af4a474862e4ca946a40111434bf18020c82251
niugaofeng <401823793@qq.com>
2020-08-17 14:38:56 +0800

    update

--- analysis/scanStructure.sh
+++ analysis/scanStructure.sh
@@ -9,11 +9,11 @@ do
       cmcalibrate --forecast 'cm/'$READ'.cm'
       cmcalibrate 'cm/'$READ'.cm'
 done
-cd cm
-cat .*cm > ../database/Template42.cm
-cd ..
+
+cat cm/*cm > database/Template42.cm
+
 cmpress database/Template42.cm
 #If a cm database already exists, please mask the above code.
 
 #search structure
-cmscan -o Sym_ITS2_core.cm database/Template42.cm "Sym_ITS2_core.fasta"
+cmscan -o Sym_ITS2_core.cm --textw 2000 database/Template42.cm "Sym_ITS2_core.fasta" 
